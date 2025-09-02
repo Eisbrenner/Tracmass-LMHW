f99061c016663f813f1764204d5f7f1c42811861
Ezra Eisbrenner <github@ezra-eisbrenner.eu>
2025-09-02 10:54:21 +0200

    Format project code with fprettify

--- projects/LMHW/read_field.F90
+++ projects/LMHW/read_field.F90
@@ -1,118 +1,118 @@
 SUBROUTINE read_field
-  !*==========================================================================
-  !*
-  !* Purpose
-  !* -------
-  !*
-  !* Read GLORYS12 model output to advect trajectories.
-  !* Will be called by the loop each time step.
-  !*
-  !* Method
-  !* ------
-  !*
-  !* Read velocities and optionally some tracers from netCDF files and
-  !* update velocity fields for TRACMASS.
-  !*
-  !* Updates the variables:
-  !*   uflux and vflux
-  !* ==========================================================================
+   !*==========================================================================
+   !*
+   !* Purpose
+   !* -------
+   !*
+   !* Read GLORYS12 model output to advect trajectories.
+   !* Will be called by the loop each time step.
+   !*
+   !* Method
+   !* ------
+   !*
+   !* Read velocities and optionally some tracers from netCDF files and
+   !* update velocity fields for TRACMASS.
+   !*
+   !* Updates the variables:
+   !*   uflux and vflux
+   !* ==========================================================================
 
-  USE mod_precdef
-  USE mod_param
-  USE mod_vel
-  USE mod_time
-  USE mod_grid
-  USE mod_getfile
-  USE mod_tracervars
-  USE mod_tracers
-  USE mod_calendar
-  USE mod_swap
+   USE mod_precdef
+   USE mod_param
+   USE mod_vel
+   USE mod_time
+   USE mod_grid
+   USE mod_getfile
+   USE mod_tracervars
+   USE mod_tracers
+   USE mod_calendar
+   USE mod_swap
 
-  USE netcdf
+   USE netcdf
 
-  IMPLICIT none
+   IMPLICIT none
 
-  INTEGER        :: kk, itrac, i0, j0, ios, jos
+   INTEGER        :: kk, itrac, i0, j0, ios, jos
 
-  REAL(DP), ALLOCATABLE, DIMENSION(:, :, :)  :: tmpu, tmpv
-  CHARACTER(len=200)                         :: fieldFile, dateprefix
+   REAL(DP), ALLOCATABLE, DIMENSION(:, :, :)  :: tmpu, tmpv
+   CHARACTER(len=200)                         :: fieldFile, dateprefix
 
-  !* Reassign the time index of uflux and vflux, dzt, dzdt, hs, ...
-  CALL swap_time()
+   !* Reassign the time index of uflux and vflux, dzt, dzdt, hs, ...
+   CALL swap_time()
 
-  !* Data files
-  dateprefix = ' '
+   !* Data files
+   dateprefix = ' '
 
-  ALLOCATE (tmpu(0:imt, 1:jmt, 1:km), tmpv(1:imt, 0:jmt, 1:km))
+   ALLOCATE (tmpu(0:imt, 1:jmt, 1:km), tmpv(1:imt, 0:jmt, 1:km))
 
-  !* Reading 3-time step variables
-  !* ===========================================================================
-  IF (ints == 0) THEN
+   !* Reading 3-time step variables
+   !* ===========================================================================
+   IF (ints == 0) THEN
 
-     !* 1 - Past
-     IF (loopYears) THEN
-        !! removed, only needed for loopYears == True
-        STOP "loopYears not implemented."
-     END IF
+      !* 1 - Past
+      IF (loopYears) THEN
+         !! removed, only needed for loopYears == True
+         STOP "loopYears not implemented."
+      END IF
 
-     !* 2 - Present
-     ! time position
-     nctstep = currDay
+      !* 2 - Present
+      ! time position
+      nctstep = currDay
 
-     dateprefix = filledFileName(dateFormat, currYear, currMon, currDay)
+      dateprefix = filledFileName(dateFormat, currYear, currMon, currDay)
 
-     !* Tracers
-     IF (l_tracers) THEN
-        !! ignore this for now
-        STOP "l_tracers not implemented."
-     END IF
+      !* Tracers
+      IF (l_tracers) THEN
+         !! ignore this for now
+         STOP "l_tracers not implemented."
+      END IF
 
-  END IF
+   END IF
 
-  IF (ints < intrun - 1 .OR. loopYears) THEN
-     !* 3 - Future
-     ! time position
-     ! this is 1 because I only have one time step per file
-     nctstep = nextDay
+   IF (ints < intrun - 1 .OR. loopYears) THEN
+      !* 3 - Future
+      ! time position
+      ! this is 1 because I only have one time step per file
+      nctstep = nextDay
 
-     dateprefix = filledFileName(dateFormat, nextYear, nextMon, nextDay)
+      dateprefix = filledFileName(dateFormat, nextYear, nextMon, nextDay)
 
-     !* Tracers
-     IF (l_tracers) THEN
-        !! ignore this for now
-        STOP "l_tracers not implemented."
-     END IF
+      !* Tracers
+      IF (l_tracers) THEN
+         !! ignore this for now
+         STOP "l_tracers not implemented."
+      END IF
 
-  END IF
+   END IF
 
-  !* Reading 2-time step variables
-  !* In this case: velocities and tracers
-  !* ===========================================================================
+   !* Reading 2-time step variables
+   !* In this case: velocities and tracers
+   !* ===========================================================================
 
-  nctstep = currDay
+   nctstep = currDay
 
-  dateprefix = filledFileName(dateFormat, currYear, currMon, currDay)
-  
-  fieldFile = TRIM(physDataDir)//TRIM(physPrefixForm)//TRIM(dateprefix)//TRIM(uGridName)//TRIM(fileSuffix)
-  tmpu(0:imt, 1:jmt, 1:km) = get3DfieldNC(fieldFile, ueul_name, [imindom, jmindom, 1, nctstep], [imt+1, jmt, km, 1], 'st')
-  
-  fieldFile = TRIM(physDataDir)//TRIM(physPrefixForm)//TRIM(dateprefix)//TRIM(vGridName)//TRIM(fileSuffix)
-  tmpv(1:imt, 0:jmt, 1:km) = get3DfieldNC(fieldFile, veul_name, [imindom, jmindom, 1, nctstep], [imt, jmt+1, km, 1], 'st')
+   dateprefix = filledFileName(dateFormat, currYear, currMon, currDay)
 
-  !* Tracers
-  IF (l_tracers) THEN
-     !! ignore this for now
-     STOP "l_tracers not implemented."
-  END IF
+   fieldFile = TRIM(physDataDir)//TRIM(physPrefixForm)//TRIM(dateprefix)//TRIM(uGridName)//TRIM(fileSuffix)
+   tmpu(0:imt, 1:jmt, 1:km) = get3DfieldNC(fieldFile, ueul_name, [imindom, jmindom, 1, nctstep], [imt + 1, jmt, km, 1], 'st')
 
-  ! uflux and vflux computation
-  FORALL (kk = 1:km) uflux(1:imt,:,kk,2) = tmpu(1:imt,:,kk)*dyu(:,:)*dzt(:,:,kk,2)
-  FORALL (kk = 1:km) vflux(:,1:jmt,kk,2) = tmpv(:,1:jmt,kk)*dxv(:,:)*dzt(:,:,kk,2)
+   fieldFile = TRIM(physDataDir)//TRIM(physPrefixForm)//TRIM(dateprefix)//TRIM(vGridName)//TRIM(fileSuffix)
+   tmpv(1:imt, 0:jmt, 1:km) = get3DfieldNC(fieldFile, veul_name, [imindom, jmindom, 1, nctstep], [imt, jmt + 1, km, 1], 'st')
 
-  !* dzdt calculation
-  dzdt = 0.d0
+   !* Tracers
+   IF (l_tracers) THEN
+      !! ignore this for now
+      STOP "l_tracers not implemented."
+   END IF
 
-  !* Reverse the sign of fluxes if trajectories are run backward in time.
-  CALL swap_sign()
+   ! uflux and vflux computation
+   FORALL (kk=1:km) uflux(1:imt, :, kk, 2) = tmpu(1:imt, :, kk)*dyu(:, :)*dzt(:, :, kk, 2)
+   FORALL (kk=1:km) vflux(:, 1:jmt, kk, 2) = tmpv(:, 1:jmt, kk)*dxv(:, :)*dzt(:, :, kk, 2)
+
+   !* dzdt calculation
+   dzdt = 0.d0
+
+   !* Reverse the sign of fluxes if trajectories are run backward in time.
+   CALL swap_sign()
 
 END SUBROUTINE read_field
--- projects/LMHW/setup_grid.F90
+++ projects/LMHW/setup_grid.F90
@@ -1,71 +1,71 @@
 SUBROUTINE setup_grid
-  !* =============================================================
-  !* Set up the grid
-  !* =============================================================
-  !* Subroutine for defining the grid of the GCM. Run once
-  !* before the loop starts.
-  !* -------------------------------------------------------------
-  !* The following arrays have to be populated:
-  !*
-  !*  dxdy - Area of horizontal cell-walls.
-  !*  dzt  - Height of k-cells in 4 dim
-  !*  kmt  - Number of k-cells from surface to seafloor.
-  !*
-  !* The following might be needed to calculate
-  !* dxdy, uflux, and vflux
-  !*
-  !*  dzu - Height of each u-gridcell.
-  !*  dzv - Height of each v-gridcell.
-  !*  dxv -
-  !*  dyu -
-  !* -------------------------------------------------------------
+   !* =============================================================
+   !* Set up the grid
+   !* =============================================================
+   !* Subroutine for defining the grid of the GCM. Run once
+   !* before the loop starts.
+   !* -------------------------------------------------------------
+   !* The following arrays have to be populated:
+   !*
+   !*  dxdy - Area of horizontal cell-walls.
+   !*  dzt  - Height of k-cells in 4 dim
+   !*  kmt  - Number of k-cells from surface to seafloor.
+   !*
+   !* The following might be needed to calculate
+   !* dxdy, uflux, and vflux
+   !*
+   !*  dzu - Height of each u-gridcell.
+   !*  dzv - Height of each v-gridcell.
+   !*  dxv -
+   !*  dyu -
+   !* -------------------------------------------------------------
 
-  USE mod_precdef
-  USE mod_param
+   USE mod_precdef
+   USE mod_param
 
-  USE mod_grid
+   USE mod_grid
 
-  IMPLICIT none
+   IMPLICIT none
 
-  INTEGER               :: jj, ii, kk
-  REAL(PP)              :: dlon, dlat, Tminlat
-  REAL(PP), ALLOCATABLE :: dz(:)
+   INTEGER               :: jj, ii, kk
+   REAL(PP)              :: dlon, dlat, Tminlat
+   REAL(PP), ALLOCATABLE :: dz(:)
 
-  !* Minimum latitude of the T grid
-  Tminlat = 9.875d0
+   !* Minimum latitude of the T grid
+   Tminlat = 9.875d0
 
-  !* Twelvs degree resolution
-  dlon = 1./12.; dlat = 1./12.
+   !* Twelvs degree resolution
+   dlon = 1./12.; dlat = 1./12.
 
-  !* dx and dy in u and v points & grid area
-  dxv(:, :) = 0.d0; dyu(:, :) = 0.d0
+   !* dx and dy in u and v points & grid area
+   dxv(:, :) = 0.d0; dyu(:, :) = 0.d0
 
-  DO jj = 1, jmt
-     DO ii = 1, imt
-        dx = dlon*deg*COS((Tminlat+0.5*dlat+dlat*(jj - 0.5))*radian)
-        dy = dlat*deg
+   DO jj = 1, jmt
+      DO ii = 1, imt
+         dx = dlon*deg*COS((Tminlat + 0.5*dlat + dlat*(jj - 0.5))*radian)
+         dy = dlat*deg
 
-        dxv(ii, jj) = dlon*deg*COS((Tminlat+0.5*dlat+dlat*jj)*radian)
-        dyu(ii, jj) = dy
+         dxv(ii, jj) = dlon*deg*COS((Tminlat + 0.5*dlat + dlat*jj)*radian)
+         dyu(ii, jj) = dy
 
-        dxdy(ii, jj) = dx*dy
+         dxdy(ii, jj) = dx*dy
 
-     END DO
-  END DO
+      END DO
+   END DO
 
-  !* Read in dz
-  ALLOCATE (dz(1:KM))
+   !* Read in dz
+   ALLOCATE (dz(1:KM))
 
-  OPEN (12, FILE=trim(topoDataDir)//trim(zgridFile))
+   OPEN (12, FILE=trim(topoDataDir)//trim(zgridFile))
 
-  DO kk = 1, KM
-     READ (12, *) dz(kk)
-  END DO
+   DO kk = 1, KM
+      READ (12, *) dz(kk)
+   END DO
 
-  CLOSE (12)
+   CLOSE (12)
 
-  do kk = 1, KM
-     dzt(:, :, kk, :) = dz(kk)
-  end do
+   do kk = 1, KM
+      dzt(:, :, kk, :) = dz(kk)
+   end do
 
 END SUBROUTINE setup_grid
